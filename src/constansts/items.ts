f7d90eb04469a2ba7c7856a4707393b99254ad32
Lokey <hahaiviet24112003@gmail.com>
2023-10-09 17:22:34 +0700

    fix data

--- src/constansts/items.ts
+++ src/constansts/items.ts
@@ -259,25 +259,24 @@ export const quizzes2 = [
 	},
 ];
 
-const lessonsEmptyArray: CardProps[] = new Array(10).fill({
+const lessonsEmpty: CardProps = {
 	navigateTo: "LessonVideo",
-	title: "Lorem ipsum",
-	id: 0,
-});
+	title: "",
+};
 
-export const lessons: CardProps[] = lessonsEmptyArray.map((lesson, index) => {
-	return {
-		...lesson,
-		id: `lesson${index}`,
+export const lessons: CardProps[] = [
+	{
+		...lessonsEmpty,
+		title: "",
+		id: `lesson0`,
 		paramList: {
-			id: `lesson${index}`,
-			videoSource: { uri: "https://pic.pikbest.com/19/58/54/88f888piCm76.mp4" },
+			id: `lesson0`,
 		},
 		avata: {
 			uri: "https://haycafe.vn/wp-content/uploads/2021/12/Hinh-anh-gau-truc-1.jpg",
 		},
-	};
-});
+	},
+];
 
 const quizEmty: CardProps = {
 	navigateTo: "Quiz",
